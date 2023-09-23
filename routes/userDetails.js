c611f37727e2a6018efa35619d971497278006d6
Rajveer <rajveer810214@gmail.com>
2023-09-23 13:59:38 +0530

    commit GNDEC EVENTS

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -5,72 +5,75 @@ const StudentInfo = require('../models/Student'); // Adjust the path as needed
 
 // Define the route handler for POST requests to add student information
 router.post('/addStudent/:id', async (req, res) => {
-  try {
-    // Extract data from the request body
-    const {
-      name,
-      email,
-      phone,
-      gender,
-      progressValue,
-      course,
-      branch,
-      urn,
-      year
-    } = req.body;
+    try {
+        // Extract data from the request body
+        const {
+            name,
+            email,
+            phone,
+            gender,
+            progressValue,
+            course,
+            branch,
+            urn,
+            crn,
+            year
+        } = req.body;
 
-    // Check if a student with the same email already exists
-    const existingEmailStudent = await StudentInfo.findOne({ email });
+        // Check if a student with the same email already exists
+        const existingEmailStudent = await StudentInfo.findOne({ email });
 
-    // Check if a student with the same phone number already exists
-    const existingPhoneStudent = await StudentInfo.findOne({ phone });
+        // Check if a student with the same phone number already exists
+        const existingPhoneStudent = await StudentInfo.findOne({ phone });
 
-    // Check if a student with the same urn already exists
-    const existingUrniStudent = await StudentInfo.findOne({ urn });
+        // Check if a student with the same urn already exists
+        const existingUrniStudent = await StudentInfo.findOne({ urn });
 
-    if (existingEmailStudent) {
-      return res.status(400).json({
-        success: false,
-        message: 'Email already exists'
-      });
+        if (existingEmailStudent) {
+            return res.status(400).json({
+                success: false,
+                // console.log(e)
+                message: 'Email already exists'
+            });
+        }
+
+        if (existingPhoneStudent) {
+            return res.status(400).json({
+                success: false,
+                message: 'Phone number already exists'
+            });
+        }
+
+        if (existingUrniStudent) {
+            return res.status(400).json({
+                success: false,
+                message: 'URN already exists'
+            });
+        }
+
+        // Create a new StudentInfo document
+        const newStudent = new StudentInfo({
+            name,
+            email,
+            phone,
+            gender,
+            progressValue,
+            course,
+            branch,
+            urn,
+            crn,
+            year,
+            eventIds: req.params.id
+        });
+
+        // Save the new student information to the database
+        const savedStudent = await newStudent.save();
+
+        res.status(201).json({ success: true, savedStudent }); // Respond with the saved student information
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error,success: false, message: 'Internal server error' });
     }
-
-    if (existingPhoneStudent) {
-      return res.status(400).json({
-        success: false,
-        message: 'Phone number already exists'
-      });
-    }
-
-    if (existingUrniStudent) {
-      return res.status(400).json({
-        success: false,
-        message: 'URN already exists'
-      });
-    }
-
-    // Create a new StudentInfo document
-    const newStudent = new StudentInfo({
-      name,
-      email,
-      phone,
-      gender,
-      progressValue,
-      course,
-      branch,
-      urn,
-      year,
-      eventIds: req.params.id
-    });
-
-    // Save the new student information to the database
-    const savedStudent = await newStudent.save();
-
-    res.status(201).json({ success: true, savedStudent }); // Respond with the saved student information
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  }
 });
 
 // Export the router
